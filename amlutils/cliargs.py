959980a87c7af256b8670ddd53641e6e90123975
smueksch <sebastian.mueksch+github@gmail.com>
2021-10-27 03:08:25 +0200

    Improving CLI argument parsing documentation.

--- amlutils/cliargs.py
+++ amlutils/cliargs.py
@@ -2,12 +2,33 @@ import argparse
 
 
 def get_cli_arguments() -> argparse.Namespace:
-    '''Return parsed comand-line arguments object.'''
+    '''
+    Return parsed comand-line arguments object.
+
+    By default, the random seed command-line argument is set to 42 and
+    the diable Comet.ml flag is set to False.
+
+    Returns:
+        argparse.Namespace: Object containing attributes with the parsed
+            command-line arguments.
+
+    Examples:
+        The return value of this function will have 3 attributes: name, seed
+        and disable_comet:
+        >>> args = get_cli_arguments()
+        >>> args.name
+        'Example name'
+        >>> args.seed
+        42
+        >>> args.disable_comet
+        False
+    '''
     parser = argparse.ArgumentParser(
         description='Process experiment parameters.')
 
     parser.add_argument('--name', help='name for experiment')
-    parser.add_argument('--seed', type=int, help='random seed for experiment')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='random seed for experiment')
     parser.add_argument(
         '--disable-comet',
         action='store_true',
