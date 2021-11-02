44ebfcd20b06aa835dfff6e079dcb92238a04234
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-02 15:23:37 +0100

    Fixing typo in GBR hyperparameter search run script.

--- task-1/gradient-boosting-regressor/run_hyperparam_grid_search.sh
+++ task-1/gradient-boosting-regressor/run_hyperparam_grid_search.sh
@@ -2,5 +2,5 @@ module load gcc/6.3.0 python/3.8.5 eth_proxy
 bsub -W 8:00 \
 -n 8 \
 -R "rusage[mem=4096]" \
--o hyperparameter_grid_search.log \
+-o hyperparam_grid_search.log \
 "python hyperparam_grid_search.py --name 'GBR (Hyperparam. Grid Search)'"
