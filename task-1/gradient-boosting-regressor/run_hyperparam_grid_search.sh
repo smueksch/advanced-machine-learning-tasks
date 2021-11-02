88c588bb26f3afe46289b518718290e1c6704993
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-02 14:48:54 +0100

    Adding ETH cluster run script for GBR hyperparameter search.

---
+++ task-1/gradient-boosting-regressor/run_hyperparam_grid_search.sh
@@ -0,0 +1,7 @@
+env2lmod
+module load gcc/6.3.0 python_cpu/3.8.5 eth_proxy
+bsub -W 8:00 \
+-n 8 \
+-R "rusage[mem=4096]" \
+-o hyperparameter_grid_search.log \
+"python hyperparam_grid_search.py --name 'GBR (Hyperparam. Grid Search)'"
