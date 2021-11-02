83c1bceaf6ef6d6377c6f60223ec9fcc2fa0e1a5
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-02 23:50:14 +0100

    Fixing job memory limit issue with GBR extended subsample grid search.

--- task-1/gradient-boosting-regressor/run_subsample_grid_search_large.sh
+++ task-1/gradient-boosting-regressor/run_subsample_grid_search_large.sh
@@ -1,6 +1,6 @@
 module load gcc/6.3.0 python/3.8.5 eth_proxy
 bsub -W 24:00 \
 -n 16 \
--R "rusage[mem=8192]" \
+-R "rusage[mem=6144]" \
 -o subsample_grid_search_large.log \
 "python subsample_grid_search_large.py --name 'GBR (Large Subsample Grid Search)'"
