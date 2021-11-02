8969a228a6d4d5236d6f4d3360de5fda667df353
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-02 23:44:05 +0100

    Adding extended GBR subsample grid searches.

--- task-1/gradient-boosting-regressor/run_subsample_grid_search.sh
+++ task-1/gradient-boosting-regressor/run_subsample_grid_search.sh
@@ -1,6 +1,6 @@
 module load gcc/6.3.0 python/3.8.5 eth_proxy
-bsub -W 8:00 \
--n 8 \
--R "rusage[mem=4096]" \
+bsub -W 24:00 \
+-n 16 \
+-R "rusage[mem=8192]" \
 -o subsample_grid_search.log \
 "python subsample_grid_search.py --name 'GBR (Subsample Grid Search)'"
