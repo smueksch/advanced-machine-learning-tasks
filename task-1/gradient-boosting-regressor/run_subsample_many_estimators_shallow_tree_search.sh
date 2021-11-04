13cda08bea91caca8268eeba86d8ad84a55223fa
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-04 13:52:20 +0100

    Fixing CPU limit issue for GBR subsample grid search.

--- task-1/gradient-boosting-regressor/run_subsample_many_estimators_shallow_tree_search.sh
+++ task-1/gradient-boosting-regressor/run_subsample_many_estimators_shallow_tree_search.sh
@@ -1,6 +1,6 @@
 module load gcc/6.3.0 python/3.8.5 eth_proxy
 bsub -W 24:00 \
--n 64 \
+-n 32 \
 -R "rusage[mem=512]" \
 -o subsample_many_estimators_shallow_tree_search.log \
 "python subsample_many_estimators_shallow_tree_search.py --name 'GBR (Many Estimators Shallow Tree Search)'"
