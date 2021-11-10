02f0323137aa00aef61922e0844d03da63db4530
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-10 22:34:32 +0100

    Fixing logging bug in GBR log feature transform search.

--- task-1/gradient-boosting-regressor/run_log_transform_hyperopt_search.sh
+++ task-1/gradient-boosting-regressor/run_log_transform_hyperopt_search.sh
@@ -2,5 +2,5 @@ module load gcc/6.3.0 python/3.8.5 eth_proxy
 bsub -W 24:00 \
 -n 16 \
 -R "rusage[mem=512]" \
--o log_transform_hyperopt_search.py \
+-o log_transform_hyperopt_search.log \
 "python log_transform_hyperopt_search.py --name 'GBR (Log. Transform Hyperopt Search)'"
