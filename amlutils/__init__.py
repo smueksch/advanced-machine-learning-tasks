2442d07ed575293fbdb6d10a5ecc909d0abe1bda
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-11 00:57:15 +0100

    Adding function to preserve DataFrame structure after transform.

--- amlutils/__init__.py
+++ amlutils/__init__.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from sklearn.base import TransformerMixin
 from typing import Sequence, Union
 
 
@@ -17,3 +19,25 @@ def get_mean(xs: Sequence[Union[int, float]]) -> float:
         float: Mean of given sequence of numbers.
     '''
     return float(np.mean(xs))
+
+
+def transform_dataframe(
+        transformer: TransformerMixin, df: pd.DataFrame) -> None:
+    '''
+    Transform DataFrame with sklearn transformer while preserving structure.
+
+    Sklearn transformers typically return NumPy arrays representing matrices
+    in the shape of the inputted DataFrame. This is an issue when further code
+    expects DataFrames and especially their functionality. This function
+    ensures that when the transformation is applied, the result is still a
+    DataFrame with the exact same structure as the input.
+
+    The provided transformer will automatically be fitted to the given
+    DataFrame.
+
+    Args:
+        transformer (TransformerMixin): Sklearn-style transformer, needs to
+            implement fit_transform.
+        df (pd.DataFrame): Pandas DataFrame that should be transformed.
+    '''
+    df.values[:] = transformer.fit_transform(df)
