46c1082eb6d625bfc39dbf6b4e67734f354c9825
smueksch <sebastian.mueksch+github@gmail.com>
2021-11-14 00:33:24 +0100

    Adding GBR grid search with outlier removal.

---
+++ task-1/gradient-boosting-regressor/run_outliers_removed_grid_search.sh
@@ -0,0 +1,6 @@
+module load gcc/6.3.0 python/3.8.5 eth_proxy
+bsub -W 24:00 \
+-n 32 \
+-R "rusage[mem=512]" \
+-o outliers_removed_grid_search.log \
+"python outliers_removed_grid_search.py --name 'GBR (Outliers Removed Search)'"
